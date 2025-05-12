2d06fb7d3c51c5bcff679611d89006dff640ed32
carlos <carlos@energetica.coop>
2025-05-12 18:42:25 +0200

    update (lun 12 may 2025 18:42:25 CEST)

---
+++ src/version.js
@@ -0,0 +1,9 @@
+function version() {
+    // v1.0.1 30/04/25 Estudio Definitivo Fotovoltaica Automatizado Individual y Colectivo
+    // fld: 1SasxRTD82q8_fJ5oJDa-vTqxA0cDEmDG
+    // DEV doc: 1Qpjg-126JvGtYxfA0bYcXq_4xmei89OJqm8-EVb-INs
+    // DEV scr: 180SVtInT_vZ1Inl27QOrTGgEDLwRTMIoeotMt0ftByva7ep5Wh4iNJrv
+    // PROD doc: 1kyX0AifhGKYSoK1aSDIMtVzBUoE8tUjStZ-HGzwdFZg
+    // PROD scr: 1FFlAvgzlYqyHZmayNWWJBzSelc0IstISc0sfQa60FHCpjLykwA-JUtD3
+  }
+  
